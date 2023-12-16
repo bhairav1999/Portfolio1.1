11a30d264cd15b3535178c9e09594c4224892865
bhairav1999 <bhairavkarande1999@gmail.com>
2023-12-16 17:47:47 +0530

    update data.jsx  change icon class name

--- src/data.jsx
+++ src/data.jsx
@@ -31,28 +31,28 @@ export const links = [
   {
     id: 1,
     name: 'Home',
-    icon: <FaHome className='nav__icon' />,
+    icon: <FaHome className='nav-icon' />,
     path: '/',
   },
 
   {
     id: 2,
     name: 'About',
-    icon: <FaUser className='nav__icon' />,
+    icon: <FaUser className='nav-icon' />,
     path: '/about',
   },
 
   {
     id: 3,
     name: 'Portfolio',
-    icon: <FaFolderOpen className='nav__icon' />,
+    icon: <FaFolderOpen className='nav-icon' />,
     path: '/portfolio',
   },
 
   {
     id: 4,
     name: 'Contact',
-    icon: <FaEnvelopeOpen className='nav__icon' />,
+    icon: <FaEnvelopeOpen className='nav-icon' />,
     path: '/contact',
   },
 ];
