90ff81075701127d979b709292d566e152d7c549
bhairav1999 <bhairavkarande1999@gmail.com>
2023-12-16 17:49:53 +0530

    navbar functinality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { links } from "../data";
+import './Navbar.css'
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [showMenu , setShowMenu]=useState(null)
 
-export default Navbar
+  return (
+    <>
+      <nav className="nav">
+        <div className={`${showMenu ? "nav-menu show-menu" : "nav-menu"}`}>
+          <ul className="nav-list">
+            {links.map(({ name, icon, path }, index) => {
+              return (
+                <li className="nav-item" key={index}>
+                  <NavLink to={path} className={({isActive})=> isActive ? 'nav-link active-nav ': 'nav-link'}
+                  onClick={()=>setShowMenu(!showMenu)}
+                  >
+                    {icon}
+                    <h3 className="nav-name">{name}</h3>
+                  </NavLink>
+                  
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className={`${showMenu ? "nav-toggle animate-toggle" : "nav-toggle"}`} onClick={()=>setShowMenu(!showMenu)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
