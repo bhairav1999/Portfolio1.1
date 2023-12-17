74b78ebb48a596fc6f93108ce9c8b683efe3a751
bhairav1999 <bhairavkarande1999@gmail.com>
2023-12-17 15:03:11 +0530

    Bhairav Home page design

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import profile from "./../../assets/home.jpeg";
+import { links } from "../../data";
+import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <section className="home section grid">
+      <img src={profile} alt="" className="home-img" />
+      <div className="home-content">
+        <div className="home-data">
+          <h1 className="home-title">
+            <span>I'm Bhairav Karande</span> Web Developer
+          </h1>
+          <p className="home-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+            atque alias ratione sit quae dignissimos nesciunt impedit
+            exercitationem harum eius nihil laborum aliquid. Rem incidunt odio,
+            commodi aspernatur perspiciatis veritatis?
+          </p>
+          <Link to="/about" className="button">
+            More About Me <span className="button-icon"></span>
+            <FaArrowRight />
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
